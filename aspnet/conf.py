2bbfb41346f0a3708fab1d5ab9239c17143011a8
Sourabh Shirhatti <shirhatti@outlook.com>
2016-03-03 15:16:04 -0800

    Add license

--- aspnet/conf.py
+++ aspnet/conf.py
@@ -47,7 +47,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'ASP.NET'
-copyright = '2015, Microsoft'
+copyright = '2016, Microsoft. Documentation licensed under CC BY 3.0'
 author = 'Microsoft'
 
 # The version info for the project you're documenting, acts as replacement for
